b1c0c9396d4a1f862702d210f077428c9c650fe8
Moises A. Cuevas <moisescuevas46@yahoo.com>
2014-11-13 11:54:39 -0800

    video 24; added comments; made it so that it connects to the database.

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,10 +5,17 @@
 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-	echo "<p> Title: $title</p>";
-	echo "<p> Post: $post</p>";
-
+	
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+		if($query){
+			echo "<p>Successfully inserted post: $title</p>";
+		}
+		else{
+			echo "<p>$connection->error</p>";
+		}
 	$connection->close();
 ?>
 <!-- echos title and post and saves -->
 <!-- makes a connection to database.php -->
+<!-- lets us insert info to our database! -->
+<!-- echos if it has connected or if it has not connected to the database to post -->
